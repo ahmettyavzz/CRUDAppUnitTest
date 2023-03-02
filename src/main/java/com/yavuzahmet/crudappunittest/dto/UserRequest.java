f168f38b4f04d8cd5895055785a42ec79ca33fd3
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-03-02 18:50:34 +0300

    create UserDto and UserRequest in dto layer

---
+++ src/main/java/com/yavuzahmet/crudappunittest/dto/UserRequest.java
@@ -0,0 +1,14 @@
+package com.yavuzahmet.crudappunittest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class UserRequest {
+    private String username;
+}
