c58559c9c43fe981aa286ef2052bcf1d069941ba
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-03-02 19:00:16 +0300

    create UserController in controller layer

---
+++ src/main/java/com/yavuzahmet/crudappunittest/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.yavuzahmet.crudappunittest.controller;
+
+import com.yavuzahmet.crudappunittest.dto.UserDto;
+import com.yavuzahmet.crudappunittest.dto.UserRequest;
+import com.yavuzahmet.crudappunittest.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto saveUser(@RequestBody UserRequest userResource) {
+        return userService.saveUser(userResource);
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable String id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @PutMapping("/{id}")
+    public UserDto updateUser(@PathVariable String id,
+                              @RequestBody UserRequest userRequest) {
+        return userService.updateUser(id, userRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUserById(@PathVariable String id) {
+        return userService.deleteUserById(id);
+    }
+}
