10f74f2140dab79633053f3dc0f57f679300c994
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-03-02 14:13:17 +0300

    Initial commit

---
+++ src/main/java/com/yavuzahmet/crudappunittest/CrudAppUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.crudappunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppUnitTestApplication.class, args);
+    }
+
+}
