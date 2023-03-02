27f6f0e4b7d3a27cc88c441a2fe33d3387688c77
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-03-02 19:00:00 +0300

    create UserService in service layer

---
+++ src/main/java/com/yavuzahmet/crudappunittest/service/UserService.java
@@ -0,0 +1,48 @@
+package com.yavuzahmet.crudappunittest.service;
+
+import com.yavuzahmet.crudappunittest.converter.UserConverter;
+import com.yavuzahmet.crudappunittest.dto.UserDto;
+import com.yavuzahmet.crudappunittest.dto.UserRequest;
+import com.yavuzahmet.crudappunittest.exception.NotFoundException;
+import com.yavuzahmet.crudappunittest.model.User;
+import com.yavuzahmet.crudappunittest.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserConverter userConverter;
+
+    public UserDto saveUser(UserRequest userRequest) {
+        var user = userConverter.toEntity(userRequest);
+        return userConverter.toDto(userRepository.save(user));
+    }
+
+    public UserDto getUser(String id) {
+        return userConverter.toDto(findUserById(id));
+    }
+
+    public List<UserDto> getAllUser() {
+        return userRepository.findAll().stream().map(userConverter::toDto).collect(Collectors.toList());
+    }
+
+    public String deleteUserById(String id){
+        findUserById(id);
+        userRepository.deleteById(id);
+        return "User deleted with this id :"+id;
+    }
+    public UserDto updateUser(String id, UserRequest userRequest){
+        var user= findUserById(id);
+        user.setUsername(userRequest.getUsername());
+        return userConverter.toDto(userRepository.save(user));
+    }
+    private User findUserById(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+    }
+}
